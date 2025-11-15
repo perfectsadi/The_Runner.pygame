4afdaf0e63386ae88477a62f02368f438f0d6c5e
perfectsadi <sadiytofficial@gmail.com>
2025-11-16 02:36:01 +0600

    Update main.py
    ok computer

--- main.py
+++ main.py
@@ -2,4 +2,29 @@ import pygame
 import sys
 #initisializing setting
 pygame.init()
-set.
+screen=pygame.display.set_mode((800,400))
+pygame.display.set_caption="The Runner.pygame"
+clock = pygame.time.Clock
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+while True:
+    clock.tick(60)
+    pygame.display.update()
+
+    #exiting the game 
+    if event.type == pygame.QUIT:
+        pygame.quit
+        exit()
