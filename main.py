2d76ca1255e778e2cfa49793a255b89bd19f33c6
perfectsadi <sadiytofficial@gmail.com>
2025-11-16 03:57:07 +0600

    ok

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-import sys
+from sys import exit
+
 #initisializing setting
 pygame.init()
 screen=pygame.display.set_mode((800,400))
@@ -12,17 +13,10 @@ score_rect=score_surface.get_rect(center=(400,100))
 sky_surface=pygame.image.load('graphics/Sky.png')
 player_surface=pygame.image.load('graphics/player/player_walk_2.png')
 player_rect= player_surface.get_rect(bottomleft=(20,300))
-
+player_gravity=0
 
 ground_surface=pygame.image.load('graphics/ground.png')
 ground_rect=ground_surface.get_rect(topleft=(0,300))
-
-
-
-
-
-
-
 
 
 
@@ -35,15 +29,18 @@ while True:
             
             pygame.quit()
             sys.exit()
-        if event.type == pygame.K_SPACE :
-            player_rect.y-=10
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE :
+                player_gravity=-20
+            
     #blit
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,ground_rect)
+    #player
+    player_gravity+=1
+    player_rect.y +=player_gravity
     screen.blit(player_surface,player_rect)
-
-    while player_rect.y >300:player_rect.y+=1
-
+    
 
     #Updating 
     pygame.display.update()
